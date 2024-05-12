9c5cb210b7164e95feb4f54c0eda27ee2130a24f
Jithendra-Bathala236 <2023.jithendrab@isu.ac.in>
2024-05-12 14:25:08 +0530

    code formatted

--- App.js
+++ App.js
@@ -1,33 +1,49 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LoginPage from './components/LoginPage';
-import SignUpPage from './components/SignUpPage';
-import ToggleButton from './components/ToggleButton';
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./pages/Login";
+import SignUpPage from "./pages/SignUp";
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  const [isLogin, setIsLogin] = React.useState(true);
-  const [navigation, setNavigation] = React.useState(null);
+    const [isLogin, setIsLogin] = useState(true);
+    const [navigation, setNavigation] = useState(null);
 
-  useEffect(() => {
-    setNavigation(Stack.navigation);
-  }, []);
+    useEffect(() => {
+        setNavigation(Stack.navigation);
+    }, []);
 
-  const toggleLogin = () => {
-    setIsLogin(!isLogin);
-  };
+    const toggleLogin = () => {
+        setIsLogin(!isLogin);
+    };
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={() => <LoginPage toggleLogin={toggleLogin} isLogin={isLogin} navigation={navigation} />} />
-        <Stack.Screen name="SignUp" component={() => <SignUpPage toggleLogin={toggleLogin} isLogin={!isLogin} navigation={navigation} />} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Login"
+                    component={() => (
+                        <Login
+                            toggleLogin={toggleLogin}
+                            isLogin={isLogin}
+                            navigation={navigation}
+                        />
+                    )}
+                />
+                <Stack.Screen
+                    name="SignUp"
+                    component={() => (
+                        <SignUpPage
+                            toggleLogin={toggleLogin}
+                            isLogin={!isLogin}
+                            navigation={navigation}
+                        />
+                    )}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default App;
